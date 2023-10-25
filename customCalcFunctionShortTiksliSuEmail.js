9da356a8502ff522c45dddf212d4c3ebd77af14b
sandravala <115172535+sandravala@users.noreply.github.com>
2023-10-25 10:33:49 +0300

    Update customCalcFunctionShortTiksliSuEmail.js

--- customCalcFunctionShortTiksliSuEmail.js
+++ customCalcFunctionShortTiksliSuEmail.js
@@ -25,7 +25,7 @@ function showResultsOrEditUrl(hideORgetValues) {
 			case 'getValues' :
 				vpaTrukme = parseInt(searchParams.get('vpaTrukme'));
 				mamaArTetisVpa = parseInt(searchParams.get('mamaArTetisVpa'));
-				naudosisNpm = searchParams.get('naudosisNpm') === 'true' ? 1 : 0;
+				naudosisNpm = searchParams.get('naudosisNpm') === 'true' ? 0 : 1;
 				mamosPajamos = parseFloat(searchParams.get('mamosPajamos'));
 				tecioPajamos = parseFloat(searchParams.get('tecioPajamos'));
 				gimdymoData = new Date(searchParams.get('gimdymoData'));
