21688b65913d59066f7980908392c40bd5f7785c
sandravala <115172535+sandravala@users.noreply.github.com>
2023-11-18 15:37:03 +0200

    Update customCalcFunctionShortTiksliSuEmail.js

--- customCalcFunctionShortTiksliSuEmail.js
+++ customCalcFunctionShortTiksliSuEmail.js
@@ -561,14 +561,13 @@ rezultatai = rezultatuLentele;
 function getAlert(fieldset, label, tevystesTarifas, motinystesTarifas, neperleidziamuMenesiuTarifas, tarifasAtostogos18men, tarifasAtostogos24men, mokesciaiNuoIsmoku, vdu, bazineSocIsmoka, motinystesIsmokaRodyti, tevystesIsmokaRodyti, vpaIsmokaRodyti, vpaTrukme, mamaArTetisVpa, naudosisNpm, mamosPajamuTipas, mamosPajamos, mamosIslaiduTipas, mamosIslaidos, tecioPajamuTipas, tecioPajamos, tecioIslaiduTipas, tecioIslaidos, emailas, gimdymoData, rezultatai, ismokuTipoLaukas, vpaTrukmesLaukas, vpaImsLaukas, npmLaukas, mamosPajamuTipoLaukas, mamosPajamuLaukas, mamosIslaiduTipoLaukas, faktiniuMamosIslaiduLaukas, tecioPajamuTipoLaukas, tecioPajamuLaukas, tecioIslaiduTipoLaukas, faktiniuTecioIslaiduLaukas, gimdymoDatosLaukas, emailoLaukas, mygtukuLaukas, rezultatuLaukas, datosInput, calcAlert, klaiduLaukas, minimumas){
 	let re = new RegExp(/(20)\d{2}-(0[1-9]|1[1,2])-(0[1-9]|[12][0-9]|3[01])/);
 
-	generateAlert(gimdymoData === '', gimdymoDatosLaukas);
-	generateAlert(!re.test(gimdymoData), gimdymoDatosLaukas, 'Įveskite gimdymo datą tinkamu formatu, t.y. "YYYY-MM-DD"');
-
 	if(tecioPajamos > 0 || mamosPajamos > 0) {
 	generateAlert(vpaTrukme === undefined, vpaTrukmesLaukas);
 	generateAlert(mamaArTetisVpa === undefined, vpaImsLaukas);
 	generateAlert(naudosisNpm === undefined, npmLaukas);
 	generateAlert(emailas === '', emailoLaukas);
+	generateAlert(gimdymoData === '', gimdymoDatosLaukas);
+	generateAlert(!re.test(gimdymoData), gimdymoDatosLaukas, 'Įveskite gimdymo datą tinkamu formatu, t.y. "YYYY-MM-DD"');
 	}
 
 	if(mamaArTetisVpa === 1 || (mamaArTetisVpa === 2 && naudosisNpm) ) {
