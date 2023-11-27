08af80953d7b0b40876209c1b7adf80f278b781c
sandravala <115172535+sandravala@users.noreply.github.com>
2023-11-28 00:35:34 +0200

    Update customCalcFunctionNewTiksli.js

--- customCalcFunctionNewTiksli.js
+++ customCalcFunctionNewTiksli.js
@@ -793,7 +793,7 @@ rezultatai = rezultatuLentele;
 
 function getAlert(fieldset, label, tevystesTarifas, motinystesTarifas, neperleidziamuMenesiuTarifas, tarifasAtostogos18men, tarifasAtostogos24men, mokesciaiNuoIsmoku, vdu, bazineSocIsmoka, motinystesIsmokaRodyti, tevystesIsmokaRodyti, vpaIsmokaRodyti, vpaTrukme, mamaArTetisVpa, naudosisNpm, mamosPajamuTipas, mamosPajamos, mamosIslaiduTipas, mamosIslaidos, tecioPajamuTipas, tecioPajamos, tecioIslaiduTipas, tecioIslaidos, gimdymoData, rezultatai, ismokuTipoLaukas, vpaTrukmesLaukas, vpaImsLaukas, npmLaukas, mamosPajamuTipoLaukas, mamosPajamuLaukas, mamosIslaiduTipoLaukas, faktiniuMamosIslaiduLaukas, tecioPajamuTipoLaukas, tecioPajamuLaukas, tecioIslaiduTipoLaukas, faktiniuTecioIslaiduLaukas, gimdymoDatosLaukas, mygtukuLaukas, rezultatuLaukas, datosInput, calcAlert, klaiduLaukas, minimumas){
 if(vpaIsmokaRodyti === 1 || motinystesIsmokaRodyti === 1 || tevystesIsmokaRodyti === 1) {
-	let re = new RegExp(/(20)\d{2}-(0[1-9]|1[1,2])-(0[1-9]|[12][0-9]|3[01])/);
+	let re = new RegExp(/(20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])/);
 
 		generateAlert(gimdymoData === '', gimdymoDatosLaukas);
 		generateAlert(!re.test(gimdymoData), gimdymoDatosLaukas, 'Įveskite gimdymo datą tinkamu formatu, t.y. "YYYY-MM-DD"');
